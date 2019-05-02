16e1242b0d4277caaadb1ce9278fca30968846dc
matthewjberger <matthewberger@nevada.unr.edu>
2019-05-01 23:53:36 -0700

    Begin adding tesselated triangle example (incomplete).

---
+++ src/06_tesselatedtriangle.rs
@@ -0,0 +1,166 @@
+use gl::types::*;
+use glfw::{Action, Context, Key};
+use std::ffi::CString;
+use std::ptr;
+
+static VERTEX_SHADER_SOURCE: &'static str = "
+#version 450 core
+
+void main(void)
+{
+    const vec4 vertices[] = vec4[](vec4( 0.25, -0.25, 0.5, 1.0),
+                                   vec4(-0.25, -0.25, 0.5, 1.0),
+                                   vec4( 0.25,  0.25, 0.5, 1.0));
+
+    gl_Position = vertices[gl_VertexID];
+}
+";
+
+static TESSELATION_CONTROL_SHADER_SOURCE: &'static str = "
+#version 450 core
+
+layout (vertices = 3) out;
+
+void main(void)
+{
+    if (gl_InvocationID == 0)
+    {
+        gl_TessLevelInner[0] = 5.0;
+        gl_TessLevelOuter[0] = 5.0;
+        gl_TessLevelOuter[1] = 5.0;
+        gl_TessLevelOuter[2] = 5.0;
+    }
+    gl_out[gl_InvocationId].gl_Position = gl_in[gl_InvocationId].gl_Position;
+}
+";
+
+static TESSELATION_EVALUATION_SHADER_SOURCE: &'static str = "
+#version 450 core
+
+layout (triangles, equal_spacing, cw) in;
+
+void main(void)
+{
+    gl_Position = (gl_TessCoord.x * gl_in[0].gl_Position) +
+                  (gl_TessCoord.y * gl_in[1].gl_Position) +
+                  (gl_TessCoord.z * gl_in[2].gl_Position);
+}
+";
+
+static FRAGMENT_SHADER_SOURCE: &'static str = "
+#version 450 core
+
+out vec4 color;
+
+void main(void)
+{
+    color = vec4(0.0, 0.8, 1.0, 1.0);
+}
+";
+
+fn main() {
+    let mut context = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    let (mut window, events) = context
+        .create_window(600, 600, "Colored Triangle", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+
+    window.make_current();
+    window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
+
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
+    let shader_program = compile_shaders();
+
+    let mut vao = 0;
+
+    unsafe {
+        gl::CreateVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+        gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
+    }
+
+    while !window.should_close() {
+        context.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            if let glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) = event {
+                window.set_should_close(true)
+            }
+            handle_window_event(&mut window, event)
+        }
+        render(shader_program);
+        window.swap_buffers();
+    }
+}
+
+fn render(shader_program: u32) {
+    let background_color: [GLfloat; 4] = [0.0, 0.0, 0.0, 1.0];
+    unsafe {
+        gl::ClearBufferfv(gl::COLOR, 0, &background_color as *const f32);
+        gl::UseProgram(shader_program);
+        gl::DrawArrays(gl::PATCHES, 0, 3);
+    }
+}
+
+fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
+    if let glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) = event {
+        window.set_should_close(true)
+    }
+}
+
+fn compile_shaders() -> GLuint {
+    let vertex_shader;
+    let fragment_shader;
+    let tesselation_control_shader;
+    let tesselation_evaluation_shader;
+    let shader_program;
+
+    let vertex_src_str = CString::new(VERTEX_SHADER_SOURCE.as_bytes()).unwrap();
+    let fragment_src_str = CString::new(FRAGMENT_SHADER_SOURCE.as_bytes()).unwrap();
+    let tesselation_control_src_str =
+        CString::new(TESSELATION_CONTROL_SHADER_SOURCE.as_bytes()).unwrap();
+    let tesselation_evaluation_src_str =
+        CString::new(TESSELATION_EVALUATION_SHADER_SOURCE.as_bytes()).unwrap();
+
+    unsafe {
+        vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
+        gl::ShaderSource(vertex_shader, 1, &vertex_src_str.as_ptr(), ptr::null());
+        gl::CompileShader(vertex_shader);
+
+        tesselation_control_shader = gl::CreateShader(gl::TESS_CONTROL_SHADER);
+        gl::ShaderSource(
+            tesselation_control_shader,
+            1,
+            &tesselation_control_src_str.as_ptr(),
+            ptr::null(),
+        );
+        gl::CompileShader(tesselation_control_shader);
+
+        tesselation_evaluation_shader = gl::CreateShader(gl::TESS_EVALUATION_SHADER);
+        gl::ShaderSource(
+            tesselation_evaluation_shader,
+            1,
+            &tesselation_evaluation_src_str.as_ptr(),
+            ptr::null(),
+        );
+        gl::CompileShader(tesselation_evaluation_shader);
+
+        fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
+        gl::ShaderSource(fragment_shader, 1, &fragment_src_str.as_ptr(), ptr::null());
+        gl::CompileShader(fragment_shader);
+
+        shader_program = gl::CreateProgram();
+        gl::AttachShader(shader_program, vertex_shader);
+        gl::AttachShader(shader_program, tesselation_control_shader);
+        gl::AttachShader(shader_program, tesselation_evaluation_shader);
+        gl::AttachShader(shader_program, fragment_shader);
+        gl::LinkProgram(shader_program);
+
+        gl::DeleteShader(vertex_shader);
+        gl::DeleteShader(tesselation_control_shader);
+        gl::DeleteShader(tesselation_evaluation_shader);
+        gl::DeleteShader(fragment_shader);
+    }
+
+    shader_program
+}

4574671546ce66a79ed54af584add4666f32d1f4
matthewjberger <matthewberger@nevada.unr.edu>
2019-05-01 22:26:03 -0700

    Add triangle example.

---
+++ src/03_triangle.rs
@@ -0,0 +1,102 @@
+use gl::types::*;
+use glfw::{Action, Context, Key};
+use std::ffi::CString;
+use std::ptr;
+
+static VERTEX_SHADER_SOURCE: &'static str = "
+#version 450 core
+void main(void) {
+    const vec4 vertices[3] = vec4[3](vec4( 0.25, -0.25, 0.5, 1.0),
+                                     vec4(-0.25, -0.25, 0.5, 1.0),
+                                     vec4( 0.25,  0.25, 0.5, 1.0));
+    gl_Position = vertices[gl_VertexID];
+}
+";
+
+static FRAGMENT_SHADER_SOURCE: &'static str = "
+#version 420 core
+out vec4 color;
+void main(void)
+{
+    color = vec4(0.0, 0.8, 1.0, 1.0);
+}
+";
+
+static RED: &'static [GLfloat; 4] = &[1.0, 0.0, 0.0, 1.0];
+
+fn main() {
+    let mut context = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    let (mut window, events) = context
+        .create_window(600, 600, "OpenGL", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+
+    window.make_current();
+    window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
+
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
+    let shader_program = compile_shaders();
+
+    let mut vao = 0;
+
+    unsafe {
+        gl::CreateVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+    }
+
+    while !window.should_close() {
+        context.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            if let glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) = event {
+                window.set_should_close(true)
+            }
+            handle_window_event(&mut window, event)
+        }
+        render(shader_program);
+        window.swap_buffers();
+    }
+}
+
+fn render(shader_program: u32) {
+    unsafe {
+        gl::ClearBufferfv(gl::COLOR, 0, RED as *const f32);
+        gl::UseProgram(shader_program);
+        gl::DrawArrays(gl::TRIANGLES, 0, 3);
+    }
+}
+
+fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
+    if let glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) = event {
+        window.set_should_close(true)
+    }
+}
+
+fn compile_shaders() -> GLuint {
+    let vertex_shader;
+    let fragment_shader;
+    let shader_program;
+
+    let vertex_src_str = CString::new(VERTEX_SHADER_SOURCE.as_bytes()).unwrap();
+    let fragment_src_str = CString::new(FRAGMENT_SHADER_SOURCE.as_bytes()).unwrap();
+
+    unsafe {
+        vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
+        gl::ShaderSource(vertex_shader, 1, &vertex_src_str.as_ptr(), ptr::null());
+        gl::CompileShader(vertex_shader);
+
+        fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
+        gl::ShaderSource(fragment_shader, 1, &fragment_src_str.as_ptr(), ptr::null());
+        gl::CompileShader(fragment_shader);
+
+        shader_program = gl::CreateProgram();
+        gl::AttachShader(shader_program, vertex_shader);
+        gl::AttachShader(shader_program, fragment_shader);
+        gl::LinkProgram(shader_program);
+
+        gl::DeleteShader(vertex_shader);
+        gl::DeleteShader(fragment_shader);
+    }
+
+    shader_program
+}

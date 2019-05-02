e0d95b236d8f8c3925b262100475c9cf66775361
Matthew J. Berger <matthewberger@nevada.unr.edu>
2019-05-02 14:23:49 -0700

    Add fragment shader example.

---
+++ src/08_fragmentshader.rs
@@ -0,0 +1,98 @@
+use gl::types::*;
+use glfw::{Action, Context, Key};
+use std::ffi::CString;
+use std::ptr;
+
+static VERTEX_SHADER_SOURCE: &'static str = "
+#version 450 core
+void main(void) {
+    const vec4 vertices[3] = vec4[3](vec4( 0.25, -0.25, 0.5, 1.0),
+                                     vec4(-0.25, -0.25, 0.5, 1.0),
+                                     vec4( 0.25,  0.25, 0.5, 1.0));
+    gl_Position = vertices[gl_VertexID];
+}
+";
+
+static FRAGMENT_SHADER_SOURCE: &'static str = "
+#version 450 core
+out vec4 color;
+void main(void)
+{
+    color = vec4(sin(gl_FragCoord.x * 0.25) * 0.5 + 0.5,
+                 cos(gl_FragCoord.y * 0.25) * 0.5 + 0.5,
+                 sin(gl_FragCoord.x * 0.15) * cos(gl_FragCoord.y * 0.15),
+                 1.0);
+}
+";
+
+static BACKGROUND_COLOR: &'static [GLfloat; 4] = &[0.0, 0.25, 0.0, 1.0];
+
+fn main() {
+    let mut context = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    let (mut window, events) = context
+        .create_window(600, 600, "Triangle", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+
+    window.make_current();
+    window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
+
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
+    let shader_program = compile_shaders();
+
+    let mut vao = 0;
+
+    unsafe {
+        gl::CreateVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+    }
+
+    while !window.should_close() {
+        context.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            if let glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) = event {
+                window.set_should_close(true)
+            }
+        }
+        render(shader_program);
+        window.swap_buffers();
+    }
+}
+
+fn render(shader_program: u32) {
+    unsafe {
+        gl::ClearBufferfv(gl::COLOR, 0, BACKGROUND_COLOR as *const f32);
+        gl::UseProgram(shader_program);
+        gl::DrawArrays(gl::TRIANGLES, 0, 3);
+    }
+}
+
+fn compile_shaders() -> GLuint {
+    let vertex_src_str = CString::new(VERTEX_SHADER_SOURCE.as_bytes()).unwrap();
+    let fragment_src_str = CString::new(FRAGMENT_SHADER_SOURCE.as_bytes()).unwrap();
+
+    let vertex_shader;
+    let fragment_shader;
+    let shader_program;
+
+    unsafe {
+        vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
+        gl::ShaderSource(vertex_shader, 1, &vertex_src_str.as_ptr(), ptr::null());
+        gl::CompileShader(vertex_shader);
+
+        fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
+        gl::ShaderSource(fragment_shader, 1, &fragment_src_str.as_ptr(), ptr::null());
+        gl::CompileShader(fragment_shader);
+
+        shader_program = gl::CreateProgram();
+        gl::AttachShader(shader_program, vertex_shader);
+        gl::AttachShader(shader_program, fragment_shader);
+        gl::LinkProgram(shader_program);
+
+        gl::DeleteShader(vertex_shader);
+        gl::DeleteShader(fragment_shader);
+    }
+
+    shader_program
+}

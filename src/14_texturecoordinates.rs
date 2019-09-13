2bda4d4982d859340d5eb59f5f0a9be492575f8b
matthewjberger <matthewberger@nevada.unr.edu>
2019-09-12 20:47:14 -0700

    Begin adding algorithmic texture

--- src/14_texturecoordinates.rs
+++ src/14_texturecoordinates.rs
@@ -12,12 +12,68 @@ const BACKGROUND_COLOR: [GLfloat; 4] = [0.0, 0.25, 0.0, 1.0];
 const GRAY: &[GLfloat; 4] = &[0.2, 0.2, 0.2, 1.0];
 const ONES: &[GLfloat; 1] = &[1.0];
 
+fn create_procedural_texture() -> u32 {
+    let (width, height) = (256 as usize, 256 as usize);
+    let mut texture = 0;
+
+    // Define data to upload into the texture
+    let mask = 0xFF;
+    let max = 255.0;
+    let mut data = vec![1.0; width * height * 4];
+    for y in 0..height {
+        for x in 0..width {
+            let index = y * width + x;
+            data[index * 4] = ((x & y) & mask) as f32 / max;
+            data[index * 4 + 1] = ((x | y) & mask) as f32 / max;
+            data[index * 4 + 2] = ((x ^ y) & mask) as f32 / max;
+            data[index * 4 + 3] = 1.0;
+        }
+    }
+
+    unsafe {
+        // Generate a name for the texture
+        gl::GenTextures(1, &mut texture);
+
+        // Bind it to the context using the GL_TEXTURE_2D binding point
+        gl::BindTexture(gl::TEXTURE_2D, texture);
+
+        // Specify the amount of storage we want to use for this texture
+        // * 2D texture
+        // * 8 mipmap levels
+        // * 32-bit floating point RGBA data
+        // * 256 x 256 texels
+        gl::TexStorage2D(gl::TEXTURE_2D, 8, gl::RGBA32F, width as i32, height as i32);
+
+        // Specify a two dimensional texture subimage
+        // * 2D texture
+        // * Level 0
+        // * Offset 0, 0
+        // * 256 x 256 texels, replace entire image
+        // * Four channel data
+        // * Floating point data
+        // * Pointer to data
+        gl::TexSubImage2D(
+            gl::TEXTURE_2D,
+            0,
+            0,
+            0,
+            256,
+            256,
+            gl::RGBA,
+            gl::FLOAT,
+            data.as_ptr() as *const GLvoid,
+        );
+    }
+    texture
+}
+
 #[derive(Default)]
 struct DemoApp {
     shader_program: ShaderProgram,
     aspect_ratio: f32,
     object: Object,
-    texture: u32,
+    texture_1: u32,
+    texture_2: u32,
 }
 
 impl DemoApp {
@@ -62,7 +118,8 @@ impl App for DemoApp {
         self.load_shaders();
 
         let (_, data) = load_ktx!("../assets/textures/pattern1.ktx").unwrap();
-        self.texture = prepare_texture(&data);
+        self.texture_1 = prepare_texture(&data);
+        self.texture_2 = create_procedural_texture();
 
         let (_, obj) = load_object!("../assets/objects/torus_nrms_tc.sbm").unwrap();
         self.object = obj;
@@ -77,7 +134,8 @@ impl App for DemoApp {
         unsafe {
             gl::ClearBufferfv(gl::COLOR, 0, GRAY as *const f32);
             gl::ClearBufferfv(gl::DEPTH, 0, ONES as *const f32);
-            gl::BindTexture(gl::TEXTURE_2D, self.texture);
+
+            gl::BindTexture(gl::TEXTURE_2D, self.texture_2);
         }
 
         self.shader_program.activate();

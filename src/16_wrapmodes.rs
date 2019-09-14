e4e74322e8a55e3de41184e07bce3ab17f8cdbec
matthewjberger <matthewberger@nevada.unr.edu>
2019-09-13 20:51:52 -0700

    Add wrapmodes example

---
+++ src/16_wrapmodes.rs
@@ -0,0 +1,82 @@
+use support::app::*;
+use support::ktx::prepare_texture;
+use support::load_ktx;
+use support::shader::*;
+
+const GREEN: &[GLfloat; 4] = &[0.0, 0.1, 0.0, 1.0];
+const YELLOW: &[GLfloat; 4] = &[0.4, 0.4, 0.0, 1.0];
+const OFFSETS: &[[GLfloat; 2]; 4] = &[[-0.5, -0.5], [0.5, -0.5], [-0.5, 0.5], [0.5, 0.5]];
+const WRAP_MODES: &[GLenum; 4] = &[
+    gl::CLAMP_TO_EDGE,
+    gl::REPEAT,
+    gl::CLAMP_TO_BORDER,
+    gl::MIRRORED_REPEAT,
+];
+
+#[derive(Default)]
+struct DemoApp {
+    shader_program: ShaderProgram,
+}
+
+impl DemoApp {
+    pub fn new() -> DemoApp {
+        DemoApp {
+            ..Default::default()
+        }
+    }
+
+    fn load_shaders(&mut self) {
+        let mut vertex_shader = Shader::new(ShaderType::Vertex);
+        vertex_shader.load_file("assets/shaders/wrapmodes/wrapmodes.vs.glsl");
+
+        let mut fragment_shader = Shader::new(ShaderType::Fragment);
+        fragment_shader.load_file("assets/shaders/wrapmodes/wrapmodes.fs.glsl");
+
+        self.shader_program = ShaderProgram::new();
+        self.shader_program
+            .attach(vertex_shader)
+            .attach(fragment_shader)
+            .link();
+
+        let mut vao = 0;
+        unsafe {
+            gl::GenVertexArrays(1, &mut vao);
+            gl::BindVertexArray(vao);
+        }
+    }
+}
+
+impl App for DemoApp {
+    fn initialize(&mut self, _: &mut glfw::Window) {
+        self.load_shaders();
+        let (_, data) = load_ktx!("../assets/textures/rightarrows.ktx").unwrap();
+        let texture = prepare_texture(&data);
+        unsafe {
+            gl::BindTexture(gl::TEXTURE_2D, texture);
+        }
+    }
+
+    fn render(&mut self, _: f32) {
+        self.shader_program.activate();
+        unsafe {
+            gl::ClearBufferfv(gl::COLOR, 0, GREEN as *const f32);
+
+            gl::TexParameterfv(
+                gl::TEXTURE_2D,
+                gl::TEXTURE_BORDER_COLOR,
+                YELLOW as *const f32,
+            );
+
+            for (index, offset) in OFFSETS.iter().enumerate() {
+                gl::Uniform2fv(0, 1, offset.as_ptr() as *const f32);
+                gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, WRAP_MODES[index] as i32);
+                gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, WRAP_MODES[index] as i32);
+                gl::DrawArrays(gl::TRIANGLE_STRIP, 0, 4);
+            }
+        }
+    }
+}
+
+fn main() {
+    DemoApp::new().run("Wrap Modes");
+}
